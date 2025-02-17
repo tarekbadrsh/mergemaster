5d340832c17b92d0f8a93b5dfd692206a3116655
tarek.abdellatef1 <tarek.abdellatef1@zeekrtech.eu>
2025-02-17 10:20:20 +0100

    feat: Enhance MergeMaster extension functionality
    - Updated vscode version in package.json
    - Added activation events for the extension
    - Implemented merge command in extension.ts with error handling
    - Created context menu and views for improved user interaction

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,85 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// /src/extension.ts:
+
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const treeDataProvider = new FileTreeProvider();
+	vscode.window.registerTreeDataProvider('mergemaster.filesView', treeDataProvider);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mergemaster" is now active!');
+	let disposable = vscode.commands.registerCommand('mergemaster.merge', async (uri: vscode.Uri) => {
+		const selectedFiles = await getSelectedFiles();
+		if (!selectedFiles || selectedFiles.length < 2) {
+			vscode.window.showErrorMessage('Please select at least 2 files to merge');
+			return;
+		}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('mergemaster.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from MergeMaster!');
+		const outputPath = await selectOutputLocation();
+		if (!outputPath) return;
+
+		try {
+			await mergeFiles(selectedFiles, outputPath);
+			vscode.window.showInformationMessage('Files merged successfully!');
+		} catch (error) {
+			vscode.window.showErrorMessage(`Error merging files: ${error}`);
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+class FileTreeProvider implements vscode.TreeDataProvider<any> {
+	getTreeItem(element: any): vscode.TreeItem | Thenable<vscode.TreeItem> {
+		return element;
+	}
+
+	getChildren(element?: any): vscode.ProviderResult<any[]> {
+		return [];
+	}
+}
+
+
+async function getSelectedFiles(): Promise<string[]> {
+	// Current incorrect implementation
+	const activeExplorer = vscode.window.activeTextEditor;
+	if (!activeExplorer) return [];
+
+	// Should be replaced with:
+	if (!vscode.workspace.workspaceFolders) {
+		return [];
+	}
+
+	return vscode.workspace.findFiles('**/*', '**/node_modules/**')
+		.then(files => files.map(file => file.fsPath));
+}
+
+async function selectOutputLocation(): Promise<string | undefined> {
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+	if (!workspaceFolders) return undefined;
+
+	const result = await vscode.window.showSaveDialog({
+		defaultUri: vscode.Uri.file(path.join(workspaceFolders[0].uri.fsPath, 'merged_output.txt')),
+		filters: {
+			'Text Files': ['txt'],
+			'All Files': ['*']
+		}
+	});
+
+	return result?.fsPath;
+}
+
+async function mergeFiles(files: string[], outputPath: string): Promise<void> {
+	let mergedContent = '';
+
+	for (const file of files) {
+		const content = await fs.promises.readFile(file, 'utf8');
+		mergedContent += `\n// File: ${path.basename(file)}\n`;
+		mergedContent += `${content}\n`;
+		mergedContent += `\n// End of ${path.basename(file)}\n`;
+	}
+
+	await fs.promises.writeFile(outputPath, mergedContent);
+}
+
+export function deactivate() { }
